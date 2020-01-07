35f278ca1a56426ea1c9b719fbcdc5e860213847
Mike <>
2020-01-07 13:57:47 +0800

    解决华为通道两次转码的问题

--- one-push-huawei/src/main/java/com/peng/one/push/huawei/hmsagents/NotificationClickActivity.java
+++ one-push-huawei/src/main/java/com/peng/one/push/huawei/hmsagents/NotificationClickActivity.java
@@ -3,17 +3,16 @@ package com.peng.one.push.huawei.hmsagents;
 import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.peng.one.push.OneRepeater;
-import com.peng.one.push.log.OneLog;
 import com.peng.one.push.utils.JsonUtils;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 
 /**
  * Created by pyt on 2017/5/15.
@@ -26,11 +25,16 @@ public class NotificationClickActivity extends Activity {
         super.onCreate(savedInstanceState);
         Uri uri = getIntent().getData();
         if (uri != null) {
+            Log.i(NotificationClickActivity.class.getSimpleName(), "URL：" + uri.toString());
             String title = uri.getQueryParameter("title");
             String content = uri.getQueryParameter("content");
-            String keyValue = uri.getQueryParameter("keyValue");
+//            String keyValue = uri.getQueryParameter("keyValue"); //因为华为通道两次转码的问题，使用新的keyValue1
+            String keyValue1 = uri.getQueryParameter("keyValue1");
             try {
-                OneRepeater.transmitNotificationClick(getApplicationContext(), -1, title, content, null, JsonUtils.toMap(new JSONObject(keyValue)));
+                Log.i(NotificationClickActivity.class.getSimpleName(), "keyValue：" + URLDecoder.decode(keyValue1, "utf-8"));
+                OneRepeater.transmitNotificationClick(getApplicationContext(), -1, title, content, null, JsonUtils.toMap(new JSONObject(URLDecoder.decode(keyValue1, "utf-8"))));
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
             } catch (JSONException e) {
                 e.printStackTrace();
             }
